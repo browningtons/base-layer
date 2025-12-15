0b386c19d541a12ccbb13948f480b6d395bed396
Paul Brown <browningtons@gmail.com>
2025-12-14 23:30:49 -0700

    please god please

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/base-layer/", // MUST match your repo name
   plugins: [react()],
-});
+  base: '/base-layer/', // 👈 must match repo name
+})
